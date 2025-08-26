034d1f4ca72d9c25ffe776054728c6e4c181039f
nguyen-khanh-duy-dev <nguyenduyyy003@gmail.com>
2025-08-26 14:35:49 +0700

    Time: 3 ms (57.37%), Space: 60.7 MB (90.93%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -10,33 +10,63 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+// var addTwoNumbers = function(l1, l2) {
+//     let arrayL1 = []
+//     let arrayL2 = []
+//     let current1 = l1
+//     let current2 = l2
+//     while(current1 !== null) {
+//         arrayL1.unshift(current1.val)
+//         current1 = current1.next
+//     }
+//     while(current2 !== null) {
+//         arrayL2.unshift(current2.val)
+//         current2 = current2.next
+//     }
+//     let result1 = BigInt(arrayL1.join(''))
+//     let result2 = BigInt(arrayL2.join(''))
+//     let result = (result1 + result2).toString().split('')
+
+
+//     let linkedList = arrayToLinkedList(result)
+//     return linkedList
+// };
+
+// function arrayToLinkedList(arr) {
+//     let head = null
+//     for (let i =0; i < arr.length; i++) {
+//         head = new ListNode(Number(arr[i]), head)
+//     }
+//     return head
+// }
+
 var addTwoNumbers = function(l1, l2) {
-    let arrayL1 = []
-    let arrayL2 = []
-    let current1 = l1
-    let current2 = l2
-    while(current1 !== null) {
-        arrayL1.unshift(current1.val)
-        current1 = current1.next
-    }
-    while(current2 !== null) {
-        arrayL2.unshift(current2.val)
-        current2 = current2.next
-    }
-    let result1 = BigInt(arrayL1.join(''))
-    let result2 = BigInt(arrayL2.join(''))
-    let result = (result1 + result2).toString().split('')
+    let result = new ListNode(0)
+    let current = result
+    let carry = 0
 
+    while(l1 !== null || l2 !== null || carry > 0) {
+        let sum = carry
 
-    let linkedList = arrayToLinkedList(result)
-    return linkedList
-};
+        if(l1 !== null) {
+            sum += l1.val
+            l1 = l1.next
+        }
 
-function arrayToLinkedList(arr) {
-    let head = null
-    for (let i =0; i < arr.length; i++) {
-        head = new ListNode(Number(arr[i]), head)
+        if(l2 !== null) {
+            sum += l2.val
+            l2 = l2.next
+        }
+
+        carry = Math.floor(sum / 10)
+        current.next = new ListNode(sum % 10)
+        current = current.next
     }
-    return head
+
+    return result.next
+
+    
+    
+
 }
 
