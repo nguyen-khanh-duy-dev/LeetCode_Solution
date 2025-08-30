edcb3f95d0d344b9256f7b47b3e3475a74ffaf99
nguyen-khanh-duy-dev <nguyenduyyy003@gmail.com>
2025-08-30 12:48:21 +0700

    Time: 1 ms (96.71%), Space: 65 MB (61.9%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if(prices.length <= 1) return 0
+    let buy = prices[0]
+    let profit = 0
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[i] < buy) {
+            buy = prices[i]
+        } else if(prices[i] - buy > profit){
+            profit = prices[i] - buy
+
+        }
+    }
+    
+    return profit
+};
