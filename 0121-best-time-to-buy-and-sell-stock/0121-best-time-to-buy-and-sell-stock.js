211b584487590215fbf9f679adefa38434359032
nguyen-khanh-duy-dev <nguyenduyyy003@gmail.com>
2025-09-04 11:35:46 +0700

    Time: 4 ms (37.26%), Space: 65.7 MB (15.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,19 +2,37 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    if(prices.length <= 1) return 0
-    let buy = prices[0]
-    let profit = 0
-    let lengthArr = prices.length
-    for(let i = 1; i < lengthArr; i++) {
-        if(prices[i] < buy) {
-            buy = prices[i]
-        } else if(prices[i] - buy > profit){
-            profit = prices[i] - buy
+// var maxProfit = function(prices) {
+//     if(prices.length <= 1) return 0
+//     let buy = prices[0]
+//     let profit = 0
+//     let lengthArr = prices.length
+//     for(let i = 1; i < lengthArr; i++) {
+//         if(prices[i] < buy) {
+//             buy = prices[i]
+//         } else if(prices[i] - buy > profit){
+//             profit = prices[i] - buy
 
-        }
-    }
+//         }
+//     }
     
-    return profit
-};
+//     return profit
+// };
+
+var maxProfit = function(prices) {
+    let profit = 0
+    let left = 0
+    let right = 1
+    let lengthPrices = prices.length
+    let maxP = 0
+    while(right < lengthPrices){
+        if(prices[left] < prices[right]) {
+            profit = prices[right] - prices[left]
+            maxP = Math.max(maxP, profit)
+        } else {
+            left = right
+        }
+        right++
+    }
+    return maxP
+}
